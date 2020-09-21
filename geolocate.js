22686bfcba3b05c94daf8e7d5705dce97de22bda
Jonathon Powell <jon@powell.gg>
2020-09-21 10:50:29 -0500

    Update geolocate.js

--- geolocate.js
+++ geolocate.js
@@ -3,6 +3,9 @@ const fetch = require("node-fetch")
 module.exports = async function(ipAddress, options){
     if(!process.env.GEOLOCATION_API_KEY) throw new Error("Missing GEOLOCATION_API_KEY")
 
+    //CloudFlare passes an IP trail. We want the client IP
+    ipAddress = ipAddress.split(",")[0]
+
     var geoRes = await fetch(`http://api.ipstack.com/${ipAddress}?access_key=${process.env.GEOLOCATION_API_KEY}`, {
         "method": "GET",
     })
